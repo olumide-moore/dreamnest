a4f97b413b208f05048bb32d4e7bb02422709b91
Mide Moore <olumide.moore2817@gmail.com>
2023-03-18 00:00:41 +0000

    Validated items in basket are within stock limit on checking out and updated stock after checkout

--- src/main/java/com/example/application/data/controllers/BasketController.java
+++ src/main/java/com/example/application/data/controllers/BasketController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 // import org.springframework.web.bind.annotation.*;
 
@@ -24,19 +25,11 @@ import java.util.List;
 
 @Controller
 public class BasketController {
-
-    // inject via application.properties
-//    @Value("${welcome.message}")
-//    private String message;
-
-    // @Autowired
     @Autowired
     private BasketService basketService;
 
     @Autowired
     private ProductService productService;
-
-
 
     @RequestMapping("/basket")
     public String basket(HttpSession session, Model model) {
@@ -60,23 +53,6 @@ public class BasketController {
            return "basket";
             }
        }
-    @PostMapping("/basket/update")
-    public String updateBasket(HttpSession session, @RequestParam("productId") Long productId, @RequestParam("quantity") Integer quantity) {
-        // User user = (User) session.getAttribute("user");
-        // if (user == null ) {
-        //     return  "redirect:/login";
-        // }else if(!(user.getRole().equals(Role.USER))){
-        //     return "redirect:/";
-        // }else{
-        if (verifyUser(session)=="") {
-            User user = (User) session.getAttribute("user");
-
-            // model.addAttribute("basket",user);
-            Basket basket = basketService.findByUserIdAndProductId(user.getId(), productId);
-            basketService.updateBasket(user.getId(), productService.findProductById(productId), quantity);
-        }
-            return ("redirect:/basket");
-    }
     public String verifyUser(HttpSession session){
         User user = (User) session.getAttribute("user");
         if (user == null ) {
@@ -88,19 +64,44 @@ public class BasketController {
         }
     }
 
+    @PostMapping("/basket/update")
+    public String updateBasket(HttpSession session, RedirectAttributes redirectAttributes, @RequestParam("productId") Long productId, @RequestParam("quantity") Integer quantity) {
+        if (verifyUser(session)=="") {
+            User user = (User) session.getAttribute("user");
+            Product product=productService.findProductById(productId);
+//            if (!enoughQuantity(product,quantity)){
+//                redirectAttributes.addFlashAttribute("error", "Sorry, not enough stock to add more quantity");
+//                return "redirect:/basket";
+//            }
+            Basket basket = basketService.findByUserIdAndProductId(user.getId(), productId);
+            basketService.updateBasket(user.getId(), productService.findProductById(productId), quantity);
+        }
+            return "redirect:/basket";
+    }
+
     @PostMapping("/basket/add")
-    public String addBasket(HttpSession session, @RequestParam("productId") Long productId, @RequestParam("quantity") Integer quantity) {
+    public String addBasket(HttpSession session, RedirectAttributes redirectAttributes, @RequestParam("productId") Long productId, @RequestParam("quantity") Integer quantity) {
        if (verifyUser(session)==""){
             User user = (User) session.getAttribute("user");
-            
+            Product product=productService.findProductById(productId);
+//            if (!enoughQuantity(product,quantity)){
+//                redirectAttributes.addFlashAttribute("error", "Sorry, not enough stock to add more quantity");
+//                return "redirect:/basket";
+//            }
             Basket basket = basketService.findByUserIdAndProductId(user.getId(), productId);
             if (basket!=null){
                 quantity = basket.getQuantity()+quantity;
             }
             basketService.updateBasket(user.getId(), productService.findProductById(productId), quantity);
        }
+        return "redirect:/basket";
+    }
 
-            return ("redirect:/basket");
+    private boolean enoughQuantity(Product product, Integer quantity){
+        if (product.getStock() >= quantity){
+            return true;
+        }
+            return false;
     }
 
 
