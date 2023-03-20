8bd9f8c67a7f73219122d9fa8edd151ff9f5d35c
aanshul10 <200240523.com>
2023-03-20 09:15:01 -0700

    search bar for products

--- src/main/resources/static/js/prodscript.js
+++ src/main/resources/static/js/prodscript.js
@@ -159,124 +159,24 @@ function addToCart(div){
     document.querySelector('.filter_option').innerHTML = `Sort by: ${text}`;
   }
 
-  //AJAX Call
-$.ajax({
-    url: 'prod.php',
-    type: 'post',
-    data: {
-        searchString: searchString
-    },
-    success: function(data) {
-        // handle the response data
-    }
-});
 
-
-  //JavaScript Function (Capture Letters)
-$('#search_bar').on('keyup', function(){
-    let searchString = $(this).val();
-    // make AJAX call
-});
-
-
-  // Display Results) from input from search bar
-function displayResults(data){
-    let html = '';
-    for(let i = 0; i < data.length; i++){
-        html += '<div class="product">';
-        html += '<img src="'+data[i].image_path+'" alt="'+data[i].name+'">';
-        html += '<h3>'+data[i].name+'</h3>';
-        html += '<p>Price: $'+data[i].price+'</p>';
-        html += '<p>Stock: '+data[i].stock+'</p>';
-        html += '</div>';
-    }
-    // display the html
-    $('#products').html(html);
-}
-
-window.onload = function () {
+function search () {
     let search = document.getElementById("search_bar");
-    let cards = document.getElementsByClassName("card-prod h-100");
+  let cards = document.getElementsByClassName("prodmain");
 
-    if (search != null) {
-        search.onkeyup = function () {
-            let letters = search.value.toLowerCase().split(''); //splits the letters of the search input into an array 
-            for (var i = 0; i < cards.length ; i++){
-                let name = cards[i].querySelector("h5.fw-bolder").textContent.toLowerCase(); //converts the product name to lowercase for comparison 
-                let nameLetters = name.split(''); //splits the letters of the product name into an array 
-                let isMatch = true; //initializes a boolean that will change to false if the input and product name don't match 
-                for (let j = 0; j < letters.length; j++){
-                    if (nameLetters[j] != letters[j]){
-                        isMatch = false; //if the input and product name don carry on with the code, set isMatch to false
-                    }
-                }
-                if (isMatch){ //if the input matches the product name, set isMatch to true
-                    cards[i].style.display = ""; //show the card 
+   if (search != null) {
 
-                } else {
-                    cards[i].style.display = "none"; //hide the card 
-                }
+        for (var i = 0; i < cards.length ; i++){
+            let name = cards[i].querySelector("h5").textContent;
+            if (name.toLowerCase().startsWith(search.value.toLowerCase())) {
+                cards[i].style.display = "";
+            } else {
+                cards[i].style.display = "none";
             }
+          }
         }
-    }
-}
+     }
 
-
-
-
-
-
-
-//window.onload = function () {
-//    let search = document.getElementbyId("search_bar");
-//   let cards = document.getElementsByClassName("card-prod h-100");
-
- //  if (search != null) {
- //   search.onkeyup = function2 () {
-  //      for (var i = 0; i < cards.length ; i++){
-   //         let name = cards[i].querySelector("h5.fw-bolder").textContent;
-   //         if (name.toLowercase().includes(search.value.toLowercase())) {
-    //            cards[i].style.display = "";
-    //        } else {
-   //             cards[i].style.display = "none";
-    //        }
-   //       }
-   //     }
-   //  }
-  // }
-   
-   
-
-
-
-
-    
-  //sortBySelect.addEventListener('change', filterNameChange);
-
- // function searcgh
-    //function search(input){
-       // document.getElementById("myInput").addEventListener(onkeyup, () =>{
-           // const form = document.createElement('form');
-           // let input = document.getElementById("myInput").innerHTML.value;
-    // form.method = 'POST';
-     //set form th:action to add-product in basket controller
-    // form.action = '/search';
-  
-    // var hiddenField = document.createElement('input');
-    // hiddenField.type = 'hidden';
-    // hiddenField.name = 'name';
-    // hiddenField.value = input;
-    // form.appendChild(hiddenField);
-   // document.body.appendChild(form);
-   // form.submit();
-    //    });
-  
-  // }
-
-   
-  //<div class="filter_option" th:text="${text}" onclick="showOptions()">Sort by</div>
-  
-  
 
 
  
