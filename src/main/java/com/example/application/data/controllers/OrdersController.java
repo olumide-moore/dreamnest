700ba833b916eb663dae5737c57ecd54cbec61d1
Mide Moore <olumide.moore2817@gmail.com>
2023-03-22 13:21:50 +0000

    Implemented and finished Orders management and orders viewing for customers

--- src/main/java/com/example/application/data/controllers/OrdersController.java
+++ src/main/java/com/example/application/data/controllers/OrdersController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -70,6 +72,18 @@ public class OrdersController
             model.addAttribute("products",products);
             model.addAttribute("users",users);
             return "edit-orders";
+        // }
+        // return page;
+    }
+
+    @PostMapping("/update-orders")
+    public String updateOrders(HttpSession session, @RequestParam("orderId") Long orderId, @RequestParam("status") String status){
+        // String page= Authorizer.verifyStaff(session);
+        // if(page==""){
+            Orders orders = ordersService.getById(orderId);
+            orders.setStatus(OrderStatus.valueOf(status));
+            ordersService.save(orders);
+            return "redirect:/edit-orders";
         // }
         // return page;
     }
