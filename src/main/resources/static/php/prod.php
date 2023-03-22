335acace27305ef8f6bec1adca93e40f3f8d9508
Mide Moore <olumide.moore2817@gmail.com>
2023-03-22 20:36:34 +0000

    Disabled all input boxes except for stock for employees, only admin can edit all inputs

--- src/main/resources/static/php/prod.php
+++
@@ -1,8 +0,0 @@
-$sql = "SELECT * FROM dreamnest.product 
-WHERE name LIKE :searchString 
-ORDER BY name ASC";
-
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(':searchString', $searchString);
-$stmt->execute();
-$result = $stmt->fetchAll();
